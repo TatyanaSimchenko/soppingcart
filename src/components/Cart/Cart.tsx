939c23431cae905c109d8a089e2a4921060956a0
Татьяна Симченко <misstaty8181@gmail.com>
2022-01-29 21:38:08 +0200

    added Items Card addItems removeItems

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,35 @@
+import CartItem from "../CartItem/CartItem";
+//types
+import { CartItemType } from "../../App";
+//Styles
+import { Wrapper } from "./Cart.styles";
+
+type Props = {
+    cartItems: CartItemType[];
+    addToCart: (clickedItem: CartItemType) => void;
+    removeFromCart: (id: number) => void;
+    removeItem: (id: number) => void;
+};
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart, removeItem}) => {
+    const calcTotal = (items: CartItemType[]) =>
+    items.reduce((acc: number, item) => acc + item.amount * item.price, 0);
+     return (
+         <Wrapper>
+             <h2>Your shopping cart</h2>
+             {cartItems.length ===0 ? <p>No items in cart</p> : null}
+             {cartItems.map(item => (
+                 <CartItem
+                 key={item.id}
+                 item={item}
+                 addToCart={addToCart}
+                 removeFromCart={removeFromCart}
+                 removeItem={removeItem}
+                  />
+             ))}
+             <h2>Total: ${calcTotal(cartItems).toFixed(2)}</h2>
+         </Wrapper>
+     )
+}
+
+export default Cart
---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,52 @@
+import { Button } from "@material-ui/core";
+//Types
+import { CartItemType } from "../../App";
+//Styles
+import { Wrapper } from "./CartItem.styles";
+
+
+type Props = {
+    item: CartItemType;
+    addToCart: (clickedItem: CartItemType) => void;
+    removeFromCart: (id: number) => void;
+    removeItem: (id: number) => void;
+}
+
+const CartItem: React.FC<Props> =({ item, addToCart, removeFromCart, removeItem }) => 
+<Wrapper>
+    <div>
+        <img src={item.image} alt={item.title} />
+        <h3>{item.title}</h3>
+        <div className="information">
+            <p>Price: ${item.price}</p>
+            <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+        </div>
+        <div className="buttons">
+            <Button 
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => removeFromCart(item.id)}>
+                -
+            </Button>
+            <p>{item.amount}</p>
+            <Button 
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => addToCart(item)}>
+                +
+            </Button>
+            <Button 
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => removeItem(item.id)}>
+                Delete
+            </Button>
+        </div>
+    </div>
+    
+</Wrapper>;
+
+export default CartItem
